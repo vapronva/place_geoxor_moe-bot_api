6e72feb452d08042229530ed7f19b00d4974bb7d
Vladimir (vapronva) <vapronhm@pm.me>
2022-04-06 23:31:16 +0300

    Add `.gitlab-ci.yml` to hopefully run the fricking CI/CD

--- publish-dc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" registry.vapronva.pw
-docker pull registry.vapronva.pw/websites/fs_geoxoplace_vapronva_pw-website:latest
-docker-compose -f 'docker-compose.yml' --project-name 'w-geoxorplacevapronvapw' down
-docker-compose -f 'docker-compose.yml' --project-name 'w-geoxorplacevapronvapw' up -d
